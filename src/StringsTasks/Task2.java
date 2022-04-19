3da07d188530431cd02eda70e95a1541670b926b
Aleksandr Grigorenko <bolgarwow@gmail.com>
2022-04-19 14:42:41 +0300

    solved task2

--- src/StringsTasks/Task2.java
+++ src/StringsTasks/Task2.java
@@ -2,7 +2,24 @@
 името ви. Въведете името си от клавиатурата.*/
 package StringsTasks;
 
+import java.util.Scanner;
+
 public class Task2
 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Please write your name");
+        String name = scanner.next();
+        System.out.println("Please write your Family name");
+        String firstName = scanner.next();
+
+        scanner.close();
+
+
+        System.out.println("Your full name is " + firstName + " "+ name);
+    }
 
 }
