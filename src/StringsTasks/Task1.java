8cc50ae6bfe2ba836d9d98344d034f18fbbeb0f1
Aleksandr Grigorenko <bolgarwow@gmail.com>
2022-04-19 13:41:49 +0300

    created task 1 class

---
+++ src/StringsTasks/Task1.java
@@ -0,0 +1,16 @@
+/*Напишете програма, която проверява дали даден символен низ
+        е префикс (начало) на друг символен низ  */
+
+
+package StringsTasks;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
+
+
