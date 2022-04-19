8714cefe0310a828d1812594f78b7be7d0982b1d
Aleksandr Grigorenko <bolgarwow@gmail.com>
2022-04-19 14:54:51 +0300

    solved task 3

--- src/StringsTasks/Task3.java
+++ src/StringsTasks/Task3.java
@@ -1,4 +1,17 @@
+/* Напишете програма, която проверява дали даден символен низ
+е палиндром (един и същ е отпред назад и отзад напред). */
+
 package StringsTasks;
 
 public class Task3 {
+
+
+    public static boolean checkStartandEndOfString (String a )
+    {
+       StringBuilder stringBuilder = new StringBuilder(a).reverse();
+
+       return a.contains(stringBuilder);
+    }
+
+
 }
