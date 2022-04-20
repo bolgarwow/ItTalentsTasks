b2af3367c56802b3fc07ecb1a48608d297836393
Aleksandr Grigorenko <bolgarwow@gmail.com>
2022-04-20 16:32:56 +0300

    completed Task 1

--- src/Figures/Task1.java
+++ src/Figures/Task1.java
@@ -1,4 +1,35 @@
+/*
+Въведете число N чрез конзолата и изкарайте като
+резултат следната матрица с височина N и широчина N
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+*/
+
 package Figures;
 
+import java.util.Scanner;
+
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        scanner.close();
+
+
+        for (int i = 0;i<a;i++)
+        {
+            String print = "";
+            for (int j =0;j<a;j++)
+            {
+                print +="# ";
+            }
+            System.out.println(print);
+        }
+    }
 }
+
